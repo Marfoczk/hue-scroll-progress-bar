fa85e3170c49c6ea397e73ee53e9d57420235d05
Marcin Głażewski <marfoczok@gmail.com>
2020-05-06 19:12:44 +0200

    added lightbox gallery

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ window.onscroll = () => {
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + "%";
     if (progress.style.height < (10+"%")) {
-        progress.style.height = `10%`
+        progress.style.height = `10%`;
     }
-    
 }
