f6e28ed9db4d4e0eec81480e2b836749fd4284ba
Marcin Głażewski <marfoczok@gmail.com>
2020-05-06 16:45:13 +0200

    upload files

---
+++ app.js
@@ -0,0 +1,11 @@
+let progress = document.querySelector('.bar');
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+window.onscroll = () => {
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+    if (progress.style.height < (10+"%")) {
+        progress.style.height = `10%`
+    }
+    
+}
